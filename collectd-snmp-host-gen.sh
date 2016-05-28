c14e06f3a1f0880dec08c68559b9899603341f9f
csepely <csepely.peter@gmail.com>
2016-05-28 17:31:22 +0200

    Update collectd-snmp-host-gen.sh

--- collectd-snmp-host-gen.sh
+++ collectd-snmp-host-gen.sh
@@ -1,5 +1,8 @@
 ip_addr=(172.16.17.98 172.16.17.102)
-for i in ${!ip_addr[@]}; do n=$(snmpget -OQ -Ov -c public -v 1 ${ip_addr[$i]} .1.3.6.1.2.1.1.5.0);(cat <<EOF
+for i in ${!ip_addr[@]}
+do
+n=$(snmpget -OQ -Ov -c public -v 1 ${ip_addr[$i]} .1.3.6.1.2.1.1.5.0)
+(cat <<EOF
 <Plugin snmp>
 <Host "$n">
 Address "${ip_addr[$i]}"
@@ -9,4 +12,5 @@ Collect "ubnt_radio_entry" "ubnt_radio_rssi_entries" "ubnt_wlstat_entry" "ubnt_a
 </Host>
 </Plugin>
 EOF
-          ) > /tmp/$(printf "%03d" "$(($i + 1))")-snmp-${n}.conf; done
+) > /tmp/$(printf "%03d" "$(($i + 1))")-snmp-${n}.conf
+done
